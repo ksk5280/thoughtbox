c083884ef9d7e4ad74bae1a1aab29bb57c9aeb51
Kimiko Kano <kskano@gmail.com>
2016-06-06 15:22:42 -0600

    Create edit button
    Toggle Read and Unread buttons
    Make text contenteditable when edit is clicked

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -11,10 +11,11 @@ function renderLink(link) {
     linkContent = '',
     $linkDiv;
 
-  linkContent += '<div class="link">';
-  linkContent += `<li>${link.title}: ${link.url}</li></div>`;
+  linkContent += `<div class="link" id="${link.id}-link">`;
+  linkContent += `<li>${link.title}: ${link.url}</li>`;
   linkContent += `<button class="btn btn-default" id="${link.id}-read">Mark as Read</button>`;
-  linkContent += `<button class="btn btn-default" id="${link.id}-unread">Mark as Unread</button>`;
+  linkContent += `<button class="btn btn-default hide" id="${link.id}-unread">Mark as Unread</button>`;
+  linkContent += `<button class="btn btn-default" id="${link.id}-edit">Edit</button></div>`;
 
   $linkDiv = $(linkContent);
   $('.links').append($linkDiv);
@@ -22,12 +23,23 @@ function renderLink(link) {
   // add Event Listeners
   $(`#${link.id}-read`).on('click', markAsRead);
   $(`#${link.id}-unread`).on('click', markAsUnread);
+  $(`#${link.id}-edit`).on('click', editLink);
 }
 
 function markAsRead() {
   $(this).prev().addClass('readLink');
+  $(this).toggleClass('hide');
+  $(this).next().toggleClass('hide');
 }
 
 function markAsUnread() {
   $(this).prev().prev().removeClass('readLink');
+  $(this).toggleClass('hide');
+  $(this).prev().toggleClass('hide');
+}
+
+function editLink() {
+  let $linkText = $(this).siblings('li');
+  $linkText.attr('contenteditable', 'true');
+  $linkText.focus();
 }
---
+++ spec/features/user_can_edit_links_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.feature "User can edit links" do
+  scenario "they see edited link" do
+    user = login_user
+    link = user.links.create(title: "Google", url: "http://www.google.com")
+
+    visit "/links"
+
+    click_on "Edit"
+  end
+end
