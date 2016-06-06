c0211b5f8bb0990e7310d276c2c342382b3e2797
Kimiko Kano <kskano@gmail.com>
2016-06-06 09:43:50 -0600

    User sign up in progress
    Create User model
    Create User controller

---
+++ spec/features/user_can_sign_in_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "User can sign in" do
+  scenario "they see user dashboard" do
+    visit "/"
+
+    expect(page).to have_content "Log In or Sign Up"
+    click_on "Sign Up"
+
+    expect(current_path).to eq new_user_path
+
+    fill_in "Email address", with: "email@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Create Account"
+
+    user = User.last
+
+    expect(current_path).to eq links_path
+    expect(page).to have_content "Welcome #{user.email}!"
+  end
+end
+
+
+# As an unauthenticated user, when I visit the root of the application, I should be redirected to a page which prompts me to "Log In or Sign Up".
+
+# If I click "Sign Up", I should be taken to a user form where I can enter an email address, a password, and a password confirmation.
+
+# I cannot sign up with an email address that has already been used.
+# Password and confirmation must match.
+# Upon submitting this information, I should be logged in via a cookie and redirected to the "Links Index" page.
